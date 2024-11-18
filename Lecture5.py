1d6eff12aaed5675698618cd2938c4d34b870b45
scientist-sanket <connectt.sanket@gmail.com>
2024-11-18 22:15:09 +0530

    New file added

---
+++ Lecture5.py
@@ -0,0 +1,37 @@
+# nums=[]
+# i=1
+# while i<=10:
+#     nums.append(i*i)
+#     i+=1
+
+# for val in nums:
+#     print(val)
+
+# nums=(1,4,9,16,25,36,49,64,81,100)
+
+# x=49
+
+# for val in nums:
+#     if(val==x):
+#         print("Number found",x)
+#         break
+#     else:
+#         print("Not found")
+
+# for el in range(1,11):
+#     print(el*8)
+
+# n=10
+# sum=0
+# i=1
+# while i<=n:
+#     sum+=i
+#     i+=1
+# print("Sum is ",sum)
+
+n=5
+fact=1
+for el in range(1,n+1):
+    fact*=el
+
+print("Factorial is ",fact)
